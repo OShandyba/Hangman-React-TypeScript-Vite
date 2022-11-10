36333d8487594a3d962d7d79f8b6a24fa741c588
Oleh Shandyba <olehshnadybagit@gmail.com>
2022-11-10 13:20:28 +0100

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,95 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { HangmanDrawing } from "./HangmanDrawing"
 import { HangmanWord } from "./HangmanWord"
 import { Keyboard } from "./Keyboard"
 import words from "./wordList.json"
 
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
+
+}
+
 function App() {
-  const [worldToGuess, setWorldToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)]
-  })
+  const [worldToGuess, setWorldToGuess] = useState(getWord)
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
+  const incorrectLetters = guessedLetters.filter(letter => !worldToGuess.includes(letter))
+
   console.log(worldToGuess)
+
+  const isLoser = incorrectLetters.length >= 6
+  const isWinner = worldToGuess.split("").every(letter => guessedLetters.includes(letter))
+
+  const addGuessedLetter = useCallback((letter: string) => {
+    if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+    setGuessedLetters(currentLetters => [...currentLetters, letter])
+  }, [guessedLetters, isLoser, isWinner])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+
+      if (!key.match(/^[a-z]$/)) return
+
+      e.preventDefault()
+      addGuessedLetter(key)
+    }
+
+    document.addEventListener('keypress', handler)
+    return () => {
+      document.removeEventListener('keypress', handler)
+    }
+  }, [guessedLetters])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+
+      if (key !== 'Enter') return
+
+      e.preventDefault()
+      setGuessedLetters([])
+      setWorldToGuess(getWord())
+    }
+
+    document.addEventListener('keypress', handler)
+    return () => {
+      document.removeEventListener('keypress', handler)
+    }
+  }, [])
+
   return (
     <div style={{
+      background: 'linear-gradient(to top left, #ffccff 29%, #cc99ff 99%)',
       maxWidth: '800px',
       display: 'flex',
       flexDirection: 'column',
       gap: '2rem',
+      padding: '30px',
       margin: '0 auto',
       alignItems: 'center'
     }}>
-      <div style={{ fontSize: '2rem', textAlign: 'center' }}>Lose Win</div>
-      <HangmanDrawing />
-      <HangmanWord />
-      <Keyboard />
+      <div style={{
+        fontSize: '2rem',
+        textAlign: 'center'
+      }}>
+        {isWinner && "Winner! - Refresh to try again"}
+        {isLoser && "Nice Try - Refresh to try again"}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={worldToGuess} />
+      <div style={{ alignSelf: 'stretch' }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter(letter =>
+            worldToGuess.includes(letter))}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter} />
+      </div>
     </div>
   )
 }
--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -72,15 +72,17 @@ const LEFT_LEG = (
         transformOrigin: 'right bottom',
     }} />
 )
-export function HangmanDrawing() {
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number
+}
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
     return (
         <div style={{ position: 'relative' }}>
-            {HEAD}
-            {BODY}
-            {RIGHT_ARM}
-            {LEFT_ARM}
-            {RIGHT_LEG}
-            {LEFT_LEG}
+            {BODY_PARTS.slice(0, numberOfGuesses)}
             <div style={{
                 height: '50px',
                 width: '10px',
@@ -91,7 +93,7 @@ export function HangmanDrawing() {
             }} />
             <div style={{
                 height: '10px',
-                width: '200px',
+                width: '150px',
                 background: 'black',
                 marginLeft: '120px',
             }} />
